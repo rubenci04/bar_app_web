dc00ec13a3b4712737f776efe9700bf17fd5adcc
rubenci04 <rea_04@hotmail.com.ar>
2025-09-14 02:53:52 -0300

    REPORTES

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,7 @@
 from functools import wraps
 from flask_login import current_user
 from flask import redirect, url_for, flash
-from datetime import datetime
+from datetime import datetime, date
 import pytz
 
 def get_current_time():
@@ -13,6 +13,14 @@ def convert_to_local_time(utc_dt, fmt=None):
     """Convierte un datetime UTC a la zona horaria local de Argentina y lo formatea si se pasa fmt."""
     if utc_dt is None:
         return '' if fmt else None
+    
+    # If it's a date object, just format it or return as is
+    if isinstance(utc_dt, date) and not isinstance(utc_dt, datetime):
+        if fmt:
+            return utc_dt.strftime(fmt)
+        return utc_dt
+
+    # If it's a datetime object, proceed with timezone conversion
     if utc_dt.tzinfo is None:
         utc_dt = pytz.utc.localize(utc_dt)
     local_tz = pytz.timezone('America/Argentina/Buenos_Aires')
